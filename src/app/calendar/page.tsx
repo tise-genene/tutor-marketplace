b5589a0a95ab54aaf8f08bfb8b31e41491204c08
tise-genene <tisegenene@gmail.com>
2025-12-04 11:48:45 +0300

    refactor(calendar): migrate from moment.js to date-fns

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,14 +1,25 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useSession } from "@/lib/auth-client";
 import { useRouter } from 'next/navigation';
 import { parseDate, toISOString, formatDateTime } from '@/lib/utils/date';
 
-const localizer = momentLocalizer(moment);
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 interface CalendarEvent {
   id: string;
